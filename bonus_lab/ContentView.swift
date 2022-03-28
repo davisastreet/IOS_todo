1e61ad3741e784dda78c89821ac95d4414403ce8
davis <davisandrewstreet@gmail.com>
2022-03-28 17:22:26 -0500

    initial commit

---
+++ bonus_lab/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  bonus_lab
+//
+//  Created by davis street on 3/28/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ bonus_lab/bonus_labApp.swift
@@ -0,0 +1,17 @@
+//
+//  bonus_labApp.swift
+//  bonus_lab
+//
+//  Created by davis street on 3/28/22.
+//
+
+import SwiftUI
+
+@main
+struct bonus_labApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
