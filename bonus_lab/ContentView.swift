105fac7ef28f17fa2255478fe9c0d756a5dfb662
Davis Street <77368064+davisastreet@users.noreply.github.com>
2022-04-22 02:07:33 -0500

    its not working

--- bonus_lab/ContentView.swift
+++ bonus_lab/ContentView.swift
@@ -12,6 +12,8 @@ struct ContentView: View
 {
     @ObservedObject var sTask = storeTask()
     @State var newTask : String = ""
+    @State private var showEdit = false
+    @State private var showDelete = false
     var searchBar : some View
     {
         HStack
@@ -23,11 +25,17 @@ struct ContentView: View
     
     func addNewTask()
     {
+        
         sTask.tasks.append(Task(id: String(sTask.tasks.count + 1), taskItem: newTask))
         self.newTask = ""
     }
+    struct dTask: Equatable
+    {
+        var i: Int
+    }
     
     var body: some View
+    
     {
         NavigationView
         {
@@ -39,7 +47,27 @@ struct ContentView: View
                     ForEach(self.sTask.tasks)
                     {
                         task in
-                        Text(task.taskItem)
+                        var i: String = task.taskItem
+                        Button(task.taskItem)
+                        {
+                            print("interacted")
+                            showEdit = true
+                        }.alert("edit task?", isPresented: $showEdit) {
+                            
+                            Button("Done", role: .cancel) { }
+                        }
+                        Button("Delete Task?")
+                        {
+                            print("interacted")
+                            showDelete = true
+                        }.alert("Task Deleted", isPresented: $showDelete) {
+                            
+                            Button("Done", role: .cancel)
+                            {
+                                let r = sTask.tasks.firstIndex(of: i)
+                                sTask.tasks.remove(at: r)
+                            }
+                        }
                     }
                 }.navigationTitle("To-Do List")
             }
@@ -51,6 +79,7 @@ struct ContentView_Previews: PreviewProvider
 {
     static var previews: some View
     {
+        
         ContentView()
     }
 }
